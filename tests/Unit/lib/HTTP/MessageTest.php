696181894ac67095e2d40589062a601fd8b9aba4
Rik Meijer <github@rikmeijer.nl>
2023-10-05 14:34:19 +0200

    headers are not _always_ in format NAME:VALUE, status and request line differ, but are headers

--- tests/Unit/lib/HTTP/MessageTest.php
+++ tests/Unit/lib/HTTP/MessageTest.php
@@ -7,13 +7,13 @@ class MessageTest extends \rikmeijer\purposeplan\Tests\Unit\TestCase {
 
     public function test_HasHeaders(): void
     {
-        $message = new \rikmeijer\purposeplan\lib\HTTP\Message(['Host' => 'example.com']);
-        $this->assertEquals('example.com', $message->headers['Host']);
+        $message = new \rikmeijer\purposeplan\lib\HTTP\Message(['Host: example.com']);
+        $this->assertEquals('Host: example.com', $message->headers[0]);
         $this->assertPropertyIsReadOnly($message, 'headers');
     }
     public function test_HasABody(): void
     {
-        $message = new \rikmeijer\purposeplan\lib\HTTP\Message(['Host' => 'example.com'], '<html>Hello World</html>');
+        $message = new \rikmeijer\purposeplan\lib\HTTP\Message([], '<html>Hello World</html>');
         $this->assertEquals('<html>Hello World</html>', $message->body);
         $this->assertPropertyIsReadOnly($message, 'body');
     }
