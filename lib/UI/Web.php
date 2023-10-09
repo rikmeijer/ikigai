71b8d307179eca0135a08ad4cb01c208ce72693f
Rik Meijer <github@rikmeijer.nl>
2023-10-09 16:26:20 +0200

    refactor web class

--- lib/UI/Web.php
+++ lib/UI/Web.php
@@ -4,20 +4,20 @@ namespace rikmeijer\purposeplan\lib\UI;
 
 class Web {
     
+    static function parseRelativeQuality(string $header_value) : array {
+        $accepting_types = array_reduce(explode(',', $header_value), function ($res, $el) {
+            list($l, $q) = array_merge(explode(';q=', $el), [1]); 
+            $res[$l] = (float) $q; 
+            return $res; 
+        }, []);
+        arsort($accepting_types);
+        return $accepting_types;
+    }
+    
     static function entry(array $server) : callable {
-        
-        $accepting_types = array_reduce(
-            explode(',', $server['HTTP_ACCEPT']), 
-            function ($res, $el) { 
-              list($l, $q) = array_merge(explode(';q=', $el), [1]); 
-              $res[$l] = (float) $q; 
-              return $res; 
-            }, []);
-          arsort($accepting_types);
-          
-        return function(callable $headers, callable $body) use ($accepting_types) {
+        return function(callable $headers, callable $body) use ($server) {
             $headers('HTTP/2 200 OK');
-            $headers('Content-Type: ' . key($accepting_types));
+            $headers('Content-Type: ' . key(self::parseRelativeQuality($server['HTTP_ACCEPT'])));
             $body('Hello World');
         };
     }
