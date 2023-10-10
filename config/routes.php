648940bc45b9c3b059a464e39dafca743896f59a
Rik Meijer <github@rikmeijer.nl>
2023-10-10 15:38:18 +0200

    fix frontend routing

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,10 @@
 <?php declare(strict_types=1);
 
 return function(callable $route) {
-    $route('', function(callable $method) {
-        $method('GET', function(callable $negotiate) {
+    $route('', function(callable $get, callable $update, callable $put, callable $delete, callable $head, callable $child) {
+        $get(function(callable $negotiate) {
             $negotiate([
                 'text/html' => fn(callable $status) => $status('200 OK', '<!DOCTYPE html>Hello World!</html>')
-            ]);
-        });
-    });
-    $route('test', function(callable $method) {
-        $method('GET', function(callable $negotiate) {
-            $negotiate([
-                'text/html' => fn(callable $status) => $status('200 OK', '<!DOCTYPE html>Hello World 2!</html>')
             ]);
         });
     });
