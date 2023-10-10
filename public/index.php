49b2ca9b0f1b92bc0aea1cf050f75f4fb74408a8
Rik Meijer <github@rikmeijer.nl>
2023-10-10 15:35:45 +0200

    cleanup index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,5 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-use function Lambdish\Phunctional\identity;
-
-rikmeijer\purposeplan\lib\UI\Web::entry($_SERVER, require dirname(__DIR__) . '/config/routes.php')('header', fn(string $body) => print $body);
+$response = rikmeijer\purposeplan\lib\UI\Web::entry($_SERVER, require dirname(__DIR__) . '/config/routes.php');
+$response('header', fn(string $body) => print $body);
