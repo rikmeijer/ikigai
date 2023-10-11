24f150943ed56179ec40a0e5df9e3f9b00b63c64
Rik Meijer <github@rikmeijer.nl>
2023-10-11 14:31:33 +0200

    rename variable to properly respond its meaning

--- lib/UI/Web.php
+++ lib/UI/Web.php
@@ -20,7 +20,7 @@ class Web {
         
         return function(callable $headers, callable $body) use ($protocol, $typesAccepted, $requestMethod, $path, $routings) : void  {
             $protocol = fn(string $code) => $headers($protocol($code));
-            $status = function(string $contentType, string $status, string $content) use ($protocol, $body, $headers) : void {
+            $respond = function(string $contentType, string $status, string $content) use ($protocol, $body, $headers) : void {
                 static $sent = false;
                 if ($sent) {
                     return;
@@ -33,8 +33,8 @@ class Web {
             
             $acceptedTypes = fn(array $availableTypes) => Functional::find(
                     fn(float $value, string $typeAccepted) => array_key_exists($typeAccepted, $availableTypes), 
-                    fn(float $value, string $typeAccepted) => Functional::partial_left($availableTypes[$typeAccepted], Functional::partial_left($status, $typeAccepted)),
-                    fn() => self::notAcceptable($status)
+                    fn(float $value, string $typeAccepted) => Functional::partial_left($availableTypes[$typeAccepted], Functional::partial_left($respond, $typeAccepted)),
+                    fn() => self::notAcceptable($respond)
             )(Functional::arsort($typesAccepted()));
             
             
@@ -50,28 +50,28 @@ class Web {
             
             $methods = Functional::map(fn($value) => Functional::partial_left($methodMatcher, strtoupper($value)))(['get', 'update', 'put', 'delete', 'head']);
 
-            $routings(self::resourceMatcher($methods, $path, $status));
+            $routings(self::resourceMatcher($methods, $path, $respond));
             
-            self::fileNotFound($status)();
+            self::fileNotFound($respond)();
         };
     }
     
-    static function resourceMatcher(array $methods, string $path, callable $status) {
+    static function resourceMatcher(array $methods, string $path, callable $respond) {
         return fn(string $identifier, callable $resource) => Functional::if_else(
             Functional::partial_left('str_starts_with', $path), 
             Functional::compose(
-                fn(string $resource_path) => $resource(...array_merge($methods, ['child' => self::resourceMatcher($methods, substr($path, strlen($resource_path)), $status)])), 
-                fn() => self::methodNotAllowed($status)(),
+                fn(string $resource_path) => $resource(...array_merge($methods, ['child' => self::resourceMatcher($methods, substr($path, strlen($resource_path)), $respond)])), 
+                fn() => self::methodNotAllowed($respond)(),
             ),
             fn() => Functional::nothing()
         )('/' . $identifier);
     }
     
-    static function notAcceptable(callable $status) : callable {
-        return fn() => $status('text/plain', '406 Not Acceptable', '');
+    static function notAcceptable(callable $respond) : callable {
+        return fn() => $respond('text/plain', '406 Not Acceptable', '');
     }
-    static function methodNotAllowed(callable $status) : callable {
-        return fn() => $status('text/plain', '405 Method Not Allowed', '');
+    static function methodNotAllowed(callable $respond) : callable {
+        return fn() => $respond('text/plain', '405 Method Not Allowed', '');
     }
     static function fileNotFound(callable $status) : callable {
         return fn() => $status('text/plain', '404 File Not Found', '');
