bef1c5ef78f4bea93af92c79347b393b73a70fbf
Rik Meijer <github@rikmeijer.nl>
2023-10-12 13:33:09 +0200

    test multiple elements

--- tests/Unit/lib/UI/TemplateTest.php
+++ tests/Unit/lib/UI/TemplateTest.php
@@ -9,13 +9,16 @@ use rikmeijer\purposeplan\lib\UI\Template;
 class TemplateTest extends \rikmeijer\purposeplan\Tests\Unit\TestCase {
     
     
-    public function test_replaceTPLElements(): void
+    public function test_replaceBlockElement(): void
     {
         $this->assertEquals('<html>Hello World</html>', Template::render('<html><block name="world" /></html>', ...['world' => fn() => 'Hello World']));
     }
     
-    public function test_replaceTPLElementsAlternative(): void
+    public function test_replaceBlockElements(): void
     {
-        $this->assertEquals('<html>Hello Universe</html>', Template::render('<html><block name="universe" /></html>', ...['universe' => fn() => 'Hello Universe']));
+        $this->assertEquals('<html>Hello WorldHello Universe</html>', Template::render('<html><block name="world" /><block name="universe" /></html>', ...[
+            'world' => fn() => 'Hello World',
+            'universe' => fn() => 'Hello Universe'
+        ]));
     }
 }
