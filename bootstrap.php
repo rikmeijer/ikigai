f2dfe91df4e252f5a1ea195a9f6f0d2199bcb7db
Rik Meijer <github@rikmeijer.nl>
2023-10-30 14:52:48 +0100

    only load .env if present

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,8 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
