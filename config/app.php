d3f07ebedf3cfeb88675c8cdbdc098d819ce8ec1
Rik Meijer <hello@rikmeijer.nl>
2024-07-08 16:07:22 +0200

    cleaning up start page

--- config/app.php
+++ config/app.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Ikigai'),
+    'tagline' => 'Planning your life goals',
+    'version' => '0.1',
 
     /*
     |--------------------------------------------------------------------------
--- tests/Feature/VersionTest.php
+++ tests/Feature/VersionTest.php
@@ -5,15 +5,15 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class VersionTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_returns_version_in_html_head(): void
     {
         $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertSeeText("Ikigai v0.1");
     }
 }
