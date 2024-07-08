136ed95dbe890bca87466991312d5041cda2cec3
Rik Meijer <hello@rikmeijer.nl>
2024-07-08 16:29:53 +0200

    test for app name and tagline being shown on welcoming page

---
+++ tests/Feature/WelcomeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class WelcomeTest extends TestCase
+{
+    public function test_the_welcome_screen_shows_version(): void
+    {
+        $response = $this->get('/');
+        $response->assertOk();
+        $response->assertSeeText("Ikigai v0.1");
+    }
+    
+    
+    public function test_the_welcome_screen_shows_name_and_tagline(): void
+    {
+        $response = $this->get('/');
+        $response->assertOk();
+        $response->assertSeeText("Ikigai: Planning your life goals");
+    }
+}
