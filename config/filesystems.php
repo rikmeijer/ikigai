6944226ddfb67161cf2b350df1e121caeb3c0e6d
Rik Meijer <hello@rikmeijer.nl>
2024-07-09 13:09:51 +0200

    since databases are volatile I don't want them in my versioned tree. There for I put them under storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'database' => [
+            'driver' => 'local',
+            'root' => storage_path('database'),
+            'throw' => false,
+        ],
+        
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
